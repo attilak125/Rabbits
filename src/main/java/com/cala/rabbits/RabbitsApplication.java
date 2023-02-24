c256cdec11213223c424cfed94241946c9b6f055
attilak125 <attila.k125@gmail.com>
2023-02-24 20:12:18 +0100

    basic

---
+++ src/main/java/com/cala/rabbits/RabbitsApplication.java
@@ -0,0 +1,13 @@
+package com.cala.rabbits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitsApplication.class, args);
+  }
+
+}
