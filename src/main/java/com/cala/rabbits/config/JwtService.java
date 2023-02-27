2147535b6f4b2a5b01714734c6bbf6d27a794b49
attilak125 <attila.k125@gmail.com>
2023-02-27 18:31:04 +0100

    Created the getSignInKey method

--- src/main/java/com/cala/rabbits/config/JwtService.java
+++ src/main/java/com/cala/rabbits/config/JwtService.java
@@ -2,10 +2,15 @@ package com.cala.rabbits.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+  private static final String SECRET_KEY = "5166546A576E5A7234753778214125432A462D4A614E645267556B5870327335";
 
   public String extractUsername(String token) {
     return null;
@@ -18,5 +23,10 @@ public class JwtService {
         .build()
         .parseClaimsJws(token)
         .getBody();
+  }
+
+  private Key getSignInKey() {
+    byte[] keyBaytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBaytes);
   }
 }
