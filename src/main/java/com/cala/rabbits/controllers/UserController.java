3971c31697c12c8baa83ac816adb9386c64080cb
attilak125 <attila.k125@gmail.com>
2023-02-27 19:24:19 +0100

    Finished the user end points

--- src/main/java/com/cala/rabbits/controllers/UserController.java
+++ src/main/java/com/cala/rabbits/controllers/UserController.java
@@ -11,14 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+  private final AuthenticationService authenticationService;
+
   @PostMapping("/register")
   public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
-
+      return ResponseEntity.ok(authenticationService.register(request));
   }
 
   @PostMapping("/login")
   public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
-
+      return ResponseEntity.ok(authenticationService.login(request));
   }
 
 }
