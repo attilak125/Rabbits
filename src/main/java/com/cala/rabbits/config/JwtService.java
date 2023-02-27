d9aff7b20d8650b1b08bf5bfd83decf31e415227
attilak125 <attila.k125@gmail.com>
2023-02-27 18:40:23 +0100

    Created the generate token method in JwtService

--- src/main/java/com/cala/rabbits/config/JwtService.java
+++ src/main/java/com/cala/rabbits/config/JwtService.java
@@ -2,10 +2,16 @@ package com.cala.rabbits.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +28,19 @@ public class JwtService {
     return claimsResolver.apply(claims);
   }
 
+  public String generateToken(UserDetails userDetails){
+    return generateToken(new HashMap<>(),userDetails);
+  }
+
+  public String generateToken(Map<String, Objects> extractClaims, UserDetails userDetails){
+    return Jwts.builder()
+        .setClaims(extractClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
   private Claims extractAllClaims(String token){
     return Jwts
         .parserBuilder()
