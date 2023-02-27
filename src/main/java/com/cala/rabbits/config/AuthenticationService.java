3022f9ae7615e2d3f3b02df74c13db656d318209
attilak125 <attila.k125@gmail.com>
2023-02-27 19:33:37 +0100

    Created the login function

--- src/main/java/com/cala/rabbits/config/AuthenticationService.java
+++ src/main/java/com/cala/rabbits/config/AuthenticationService.java
@@ -6,6 +6,9 @@ import com.cala.rabbits.models.RegisterRequest;
 import com.cala.rabbits.models.Role;
 import com.cala.rabbits.models.User;
 import com.cala.rabbits.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +17,14 @@ public class AuthenticationService {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
   private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
 
   public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder,
-      JwtService jwtService) {
+      JwtService jwtService, AuthenticationManager authenticationManager) {
     this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
     this.jwtService = jwtService;
+    this.authenticationManager = authenticationManager;
   }
 
   public AuthenticationResponse register(RegisterRequest request) {
@@ -37,6 +42,12 @@ public class AuthenticationService {
 
 
   public AuthenticationResponse login(AuthenticationRequest request) {
-    return null;
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail(),
+            request.getPassword()));
+    var user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User name not found"));
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder().token(jwtToken).build();
   }
 }
