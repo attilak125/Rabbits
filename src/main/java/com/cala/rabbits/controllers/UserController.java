9607ebcbe61589f010947b07cb3172ff1f349985
attilak125 <attila.k125@gmail.com>
2023-02-27 19:17:36 +0100

    Created the User controller with register and login endpoints

---
+++ src/main/java/com/cala/rabbits/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.cala.rabbits.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request){
+
+  }
+
+}
