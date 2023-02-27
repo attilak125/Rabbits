6f0bd2c3234dfbcc0739babc3174d2a49fff204e
attilak125 <attila.k125@gmail.com>
2023-02-27 19:20:56 +0100

    Created the RegisterRequest class

--- src/main/java/com/cala/rabbits/controllers/UserController.java
+++ src/main/java/com/cala/rabbits/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.cala.rabbits.controllers;
 
 import com.cala.rabbits.models.AuthenticationResponse;
+import com.cala.rabbits.models.RegisterRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
