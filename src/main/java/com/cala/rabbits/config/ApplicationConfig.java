0e1da1a0e78bf7f33d3fa7a6a538eea2f7cc1611
attilak125 <attila.k125@gmail.com>
2023-02-27 18:53:49 +0100

    Created the application config class and the load user by username function;

---
+++ src/main/java/com/cala/rabbits/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.cala.rabbits.config;
+
+import com.cala.rabbits.repositories.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class ApplicationConfig {
+  private final UserRepository userRepository;
+
+  public ApplicationConfig(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(){
+    return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+}
